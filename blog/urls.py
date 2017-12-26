11853db2002e92e6f4c52da7c30b7aa43b610a0d
wangyawei9999 <wangyawei9999@gmail.com>
2017-12-27 00:14:06 +0900

    add edit and detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
+    path('new/', views.post_new, name='post_new'),
+    path('post/<pk>/edit/', views.post_edit, name='post_edit'),
 ]
